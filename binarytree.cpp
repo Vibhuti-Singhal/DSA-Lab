ba7633f65b9da5d118106b1db078b6485f0dd93a
Vibhuti-Singhal <singhalvibhuti09@gmail.com>
2025-11-22 00:14:11 +0530

    Add files via upload

---
+++ binarytree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+const int MAX = 100;   // Maximum size of array for tree
+void insertNode(int tree[], int index, int value) {
+    if (index >= MAX) {
+        cout << "Insertion failed! Index out of range.\n";
+        return;
+    }
+    tree[index] = value;
+}
+void printTree(int tree[], int size) {
+    cout << "Binary Tree (Array Representation): ";
+    for (int i = 0; i < size; i++) {
+        cout << tree[i] << " ";
+    }
+    cout << endl;
+}
+void createTree(int tree[], int size) {
+    for (int i = 0; i < size; i++) {
+        int value;
+        cout << "Enter value for node at index " << i << ": ";
+        cin >> value;
+        insertNode(tree, i, value);
+    }
+}
+int main() {
+    int tree[MAX];
+        for (int i = 0; i < MAX; i++) // Initializing array with -1 (meaning empty)
+            tree[i] = -1;
+    int n;
+    cout << "Enter number of nodes to insert in binary tree: ";
+    cin >> n;
+    createTree(tree, n);
+    printTree(tree, n);
+    return 0;
+}
+
